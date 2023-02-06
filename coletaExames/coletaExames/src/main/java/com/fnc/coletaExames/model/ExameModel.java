1b37324dba639a6928389a07872b91bd6c4886fb
Marcelo Delgado <101508582+codecelo@users.noreply.github.com>
2023-02-06 20:27:07 -0300

    DataColeta

--- coletaExames/coletaExames/src/main/java/com/fnc/coletaExames/model/ExameModel.java
+++ coletaExames/coletaExames/src/main/java/com/fnc/coletaExames/model/ExameModel.java
@@ -4,14 +4,34 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 
 public class ExameModel {
+    private Date dataColeta;
+
     private PacienteModel paciente;
     private List<ProcedimentoModel> procedimentos;
 
-    }
+    public Date getDataEntrega() {
+        var prazoMaximo = Collections.max(procedimentos.stream()
+                .map(ProcedimentoModel::getPrazoMaximo)
+                .toList());
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dataColeta);
 
+        calendar.add(Calendar.DATE, prazoMaximo);
+
+        if(calendar.get(Calendar.DAY_OF_WEEK) == 7)
+            calendar.add(Calendar.DATE, 1);
+        else if(calendar.get(Calendar.DAY_OF_WEEK) == 6)
+            calendar.add(Calendar.DATE, 2);
+        return calendar.getTime();
+
+    }
+}
